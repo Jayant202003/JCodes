da2a4deca027adf41da43dbb0a565c634c3638d9
Jayant Shivankar <jayantshivankar4@gmail.com>
2026-08-22 23:59:07 +0530

    Time: 0 ms (100%), Space: 8 MB (42.87%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numTrees(int n) {
-         vector<int> dp(n + 1, 0);
-        dp[0] = 1; 
-        dp[1] = 1; 
+          vector<int> dp(n + 1, 0);
+        dp[0] = 1; // empty tree is one valid "shape"
+        dp[1] = 1; // single node is one valid tree
 
         for (int nodes = 2; nodes <= n; nodes++) {
             for (int root = 1; root <= nodes; root++) {
-                int leftCount = root - 1;        
-                int rightCount = nodes - root;   
+                int leftCount = root - 1;        // nodes in left subtree
+                int rightCount = nodes - root;    // nodes in right subtree
                 dp[nodes] += dp[leftCount] * dp[rightCount];
             }
         }
